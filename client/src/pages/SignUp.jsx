fe88b64b8151a1d7ecb621afc1adfc6cbdf9765d
Tharika Akurana <152663116+Tharika-Akurana@users.noreply.github.com>
2024-05-09 15:43:33 -0700

    Update SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -5,8 +5,9 @@ export default function SignUp() {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id = 'username' /> 
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id = 'email' />
+        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id = 'username' /> 
+        <input type="email" placeholder='E-mail' className='border p-3 rounded-lg' id = 'email' />
+        <input type="password" placeholder='Enter Password' className='border p-3 rounded-lg' id = 'enter password' />
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id = 'password' />
         <button className='bg-slate-800 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
