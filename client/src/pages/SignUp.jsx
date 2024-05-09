b86c02125fe5859c7ba6c28aaa1c7c5b1953ac57
Tharika Akurana <152663116+Tharika-Akurana@users.noreply.github.com>
2024-05-09 15:59:11 -0700

    Complete Sign Up Page UI Updated

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -8,13 +8,13 @@ export default function SignUp() {
         <input type="text" placeholder='Username' className='border p-3 rounded-lg' id = 'username' /> 
         <input type="email" placeholder='E-mail' className='border p-3 rounded-lg' id = 'email' />
         <input type="password" placeholder='Enter Password' className='border p-3 rounded-lg' id = 'enter password' />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id = 'password' />
+        <input type="password" placeholder='Confirm Password' className='border p-3 rounded-lg' id = 'password' />
         <button className='bg-slate-800 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
-          <span className="text-blue-700 bg">Sign in</span>
+          <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
     </div>
