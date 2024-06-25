d9d9534f2167c7f8e88d68db29528283e2afe368
Tharika Akurana <152663116+Tharika-Akurana@users.noreply.github.com>
2024-06-25 11:36:19 -0700

    Updated Header Section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,64 +1,57 @@
-import {FaSearch} from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function Header() {
     const { currentUser } = useSelector((state) => state.user);
-  return (
-    <header className='bg-slate-200 shadow-md'>
 
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-
-            <Link to='/'>
-                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-
-                    <span className='text-slate-500'>
-                        Hotel
-                    </span>
-
-                    <span className='text-slate-700'>
-                        Hub
-                    </span>
-
-                </h1>
-            </Link>
-
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-
-                <input type="text" placeholder="Search..." 
-                className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-                
-                <FaSearch className='text-slate-600'/>
-            </form>
-            
-            <ul className='flex gap-4'>
-
-                <Link to  = '/'>
-                <li className='hidden sm:inline text-slate-700 hover:underline'>
-                    Home
-                </li>
+    return (
+        <header className='bg-slate-200 shadow-md'>
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-col items-center'>
+                        <span className='text-slate-500'>
+                            Stay&Dine
+                        </span>
+                        <span className='text-slate-700'>
+                            Hub
+                        </span>
+                    </h1>
                 </Link>
 
-                <Link to = '/about'>
-                <li className='hidden sm:inline text-slate-700 hover:underline'>
-                    About
-                </li>
-                </Link>
+                <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className='bg-transparent focus:outline-none w-24 sm:w-64'
+                    />
+                    <FaSearch className='text-slate-600' />
+                </form>
 
-                <Link to='/profile'>
-                {currentUser ? (
-                <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
-                ) : (
-                <li className=' text-slate-700 hover:underline'> 
-                    Sign in
-                </li>
-                )}
-                </Link>
-
-            </ul>
-
-        </div>
-
-    </header>
-  )
+                <ul className='flex gap-4'>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li className='hidden sm:inline text-slate-700 hover:underline'>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/profile'>
+                            {currentUser ? (
+                                <img
+                                    className='rounded-full h-7 w-7 object-cover'
+                                    src={currentUser.avatar}
+                                    alt='profile'
+                                />
+                            ) : (
+                                <span className='text-slate-700 hover:underline'>
+                                    Sign in
+                                </span>
+                            )}
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    );
 }
