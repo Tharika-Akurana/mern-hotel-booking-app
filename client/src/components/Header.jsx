46a0650d96c2ba67612ff67cf465d7e9df6c0f9c
Tharika Akurana <152663116+Tharika-Akurana@users.noreply.github.com>
2024-10-19 13:10:07 +0530

    Update Booking Form UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,75 +1,77 @@
-import { FaSearch } from 'react-icons/fa';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import logo from '../assets/Images/Logo.png';
+import { FaSearch } from "react-icons/fa";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import logo from "../assets/Images/Logo.png";
 
 export default function Header() {
-    const { currentUser } = useSelector((state) => state.user);
-    const location = useLocation();
-    const showHeader = location.pathname !== '/'&& location.pathname !== '/admin-sign-in' && 
-    location.pathname !== '/sign-in' && location.pathname !== '/sign-up' && location.pathname !== '/create-listings' 
-    && location.pathname !== '/listings' && location.pathname !== '/edit-listings' && location.pathname !== '/logout' 
-    && location.pathname !== '/boutique-hotels' && location.pathname !== '/budget-hotels' && location.pathname !== '/business-hotels' 
-    && location.pathname !== '/guest-houses' && location.pathname !== '/luxury-hotels' && location.pathname !== '/resorts'
-    && location.pathname !== '/restaurants' && location.pathname !== '/transient-hotels' &&  
-    !location.pathname.startsWith('/hotel-details/') && !location.pathname.startsWith('/edit-listing/') ;
-    const navigate = useNavigate;
-    
-  
-    if (!showHeader) return null;
+  const { currentUser } = useSelector((state) => state.user);
+  const location = useLocation();
+  const showHeader =
+    location.pathname !== "/" &&
+    location.pathname !== "/admin-sign-in" &&
+    location.pathname !== "/sign-in" &&
+    location.pathname !== "/sign-up" &&
+    location.pathname !== "/create-listings" &&
+    location.pathname !== "/listings" &&
+    location.pathname !== "/edit-listings" &&
+    location.pathname !== "/logout" &&
+    !location.pathname.startsWith("/edit-listing/");
+  const navigate = useNavigate;
 
-    return (
-        <header className='bg-DeepNavyBlue shadow-lg'>
-            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-                <Link to='/'>
-                    <div className='flex items-center'>
-                        <img src={logo} alt="Logo" className='w-14 h-14 mr-4 rounded-full' />
-                        <h1 className='font-bold text-sm sm:text-xl flex flex-col items-center text-slate-300 hover:text-darkGold'>
-                            <span className='text-center'>
-                                Stay&Dine
-                            </span>
-                            <span className='text-center'>
-                                Hub
-                            </span>
-                        </h1>
-                    </div>
-                </Link>
+  if (!showHeader) return null;
 
-                <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        className='bg-transparent focus:outline-none w-24 sm:w-64'
-                    />
-                    <FaSearch className='text-slate-600' />
-                </form>
+  return (
+    <header className="bg-DeepNavyBlue shadow-lg">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <div className="flex items-center">
+            <img
+              src={logo}
+              alt="Logo"
+              className="w-14 h-14 mr-4 rounded-full"
+            />
+            <h1 className="font-bold text-sm sm:text-xl flex flex-col items-center text-slate-300 hover:text-darkGold">
+              <span className="text-center">Stay&Dine</span>
+              <span className="text-center">Hub</span>
+            </h1>
+          </div>
+        </Link>
 
-                <ul className='flex gap-4'>
-                    <li className='hidden sm:inline text-white hover:text-goldenYellow  hover:underline'>
-                        <Link to='/Home'>Home</Link>
-                    </li>
-                    <li className='hidden sm:inline text-white hover:text-goldenYellow hover:underline'>
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li>
-                            {currentUser ? (
-                                <Link to='/profile'>
-                                <img
-                                    className='rounded-full h-7 w-7 object-cover'
-                                    src={currentUser.avatar}
-                                    alt='profile'
-                                />
-                                </Link>
-                            ) : (
-                                <Link to='/sign-in'>
-                                <span className='text-white hover:text-goldenYellow hover:underline' >
-                                    Sign in
-                                </span>
-                                </Link>
-                            )}
-                    </li>
-                </ul>
-            </div>
-        </header>
-    );
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+
+        <ul className="flex gap-4">
+          <li className="hidden sm:inline text-white hover:text-goldenYellow  hover:underline">
+            <Link to="/Home">Home</Link>
+          </li>
+          <li className="hidden sm:inline text-white hover:text-goldenYellow hover:underline">
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            {currentUser ? (
+              <Link to="/profile">
+                <img
+                  className="rounded-full h-7 w-7 object-cover"
+                  src={currentUser.avatar}
+                  alt="profile"
+                />
+              </Link>
+            ) : (
+              <Link to="/sign-in">
+                <span className="text-white hover:text-goldenYellow hover:underline">
+                  Sign in
+                </span>
+              </Link>
+            )}
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
--- client/src/pages/BookingForm.jsx
+++ client/src/pages/BookingForm.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 const BookingForm = () => {
   const [hotelType, setHotelType] = useState("");
   const [roomTypeVisible, setRoomTypeVisible] = useState(true);
+  const [stayOrFunction, setStayOrFunction] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("");
 
   const handleHotelTypeChange = (e) => {
     const selectedHotelType = e.target.value;
@@ -10,64 +12,63 @@ const BookingForm = () => {
     setRoomTypeVisible(selectedHotelType !== "restaurant");
   };
 
+  const handlePaymentMethodChange = (e) => {
+    setPaymentMethod(e.target.value);
+  };
+
   return (
-    <div className="bg-gray-100 p-8 max-w-lg mx-auto rounded-md shadow-md">
-      <h2 className="text-2xl font-bold text-center mb-6">
-        Hotel Booking Form
+    <div className="bg-cream p-8 w-2/3 md:w-6/12 mx-auto rounded-md shadow-md mt-5">
+      <h2 className="text-2xl font-sans font-bold text-center mb-6">
+        Hotel Reservation Form
       </h2>
+      <p className="text-sm text-center mb-8">
+        Tell us your preferences for a tailored booking experience that
+        guarantees a memorable stay for dining, business, or special events.
+      </p>
       <form>
         {/* Guest Details */}
-        <div className="mb-4">
-          <label htmlFor="guestName" className="block text-gray-700">
-            Full Name:
-          </label>
+        <div className="flex">
+          <label className="block font-serif w-1/3 mb-3">Full Name:</label>
           <input
             type="text"
             id="guestName"
             name="guestName"
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+            className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
             required
           />
         </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700">
-            Email Address:
-          </label>
+        <div className="flex">
+          <label className="block font-serif w-1/3 mb-3">Email Address: </label>
           <input
             type="email"
             id="email"
             name="email"
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+            className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
             required
           />
         </div>
-        <div className="mb-4">
-          <label htmlFor="phone" className="block text-gray-700">
-            Phone Number:
-          </label>
+        <div className="flex">
+          <label className="block font-serif w-1/3 mb-3">Phone Number:</label>
           <input
             type="tel"
             id="phone"
             name="phone"
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+            className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
             required
           />
         </div>
-
         {/* Hotel Type Selection */}
-        <div className="mb-4">
-          <label htmlFor="hotelType" className="block text-gray-700">
-            Select Hotel Type:
-          </label>
+        <div className="flex">
+          <label className="block font-serif w-1/3 mb-3">Hotel Type:</label>
           <select
             id="hotelType"
             name="hotelType"
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+            className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
             value={hotelType}
             onChange={handleHotelTypeChange}
             required
           >
-            <option value="">--Select Hotel Type--</option>
+            <option value="">Select Hotel Type</option>
             <option value="restaurant">Restaurant</option>
             <option value="resort">Resort</option>
             <option value="budget">Budget Hotel</option>
@@ -79,45 +80,150 @@ const BookingForm = () => {
           </select>
         </div>
 
-        {/* Check-in and Check-out Dates */}
-        <div className="mb-4">
-          <label htmlFor="checkIn" className="block text-gray-700">
-            Check-In Date:
-          </label>
-          <input
-            type="date"
-            id="checkIn"
-            name="checkIn"
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="checkOut" className="block text-gray-700">
-            Check-Out Date:
-          </label>
-          <input
-            type="date"
-            id="checkOut"
-            name="checkOut"
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
-            required
-          />
-        </div>
+        {/* Stay or Function Option for Resort, Boutique and Luxury Hotels*/}
+        {["resort", "boutique", "luxury"].includes(hotelType) && (
+          <div className="flex">
+            <label className="block font-serif w-1/3 mb-3">
+              Stay or Function:
+            </label>
+            <select
+              value={stayOrFunction}
+              onChange={(e) => setStayOrFunction(e.target.value)}
+              className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+              required
+            >
+              <option value="">Select Option</option>
+              <option value="stay">Stay</option>
+              <option value="function">Function</option>
+            </select>
+          </div>
+        )}
+
+        {/* Conditional Check-in and Check-out Dates or Function Selection */}
+        {stayOrFunction === "stay" ? (
+          <>
+            <div className="flex">
+              <label className="block font-serif w-1/3 mb-3">
+                Check-In Date:
+              </label>
+              <input
+                type="date"
+                id="checkIn"
+                name="checkIn"
+                className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+            <div className="flex">
+              <label className="block font-serif w-1/3 mb-3">
+                Check-Out Date:
+              </label>
+              <input
+                type="date"
+                id="checkOut"
+                name="checkOut"
+                className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+          </>
+        ) : stayOrFunction === "function" ? (
+          <>
+            <div className="flex">
+              <label className="block font-serif w-1/3 mb-3">
+                Function Type:
+              </label>
+              <select
+                id="functionType"
+                name="functionType"
+                className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+                required
+              >
+                <option value="">Select Function Type</option>
+                <option value="day">Day Function</option>
+                <option value="night">Night Function</option>
+              </select>
+            </div>
+            <div className="flex">
+              <label className="block font-serif w-1/3 mb-3">Date:</label>
+              <input
+                type="date"
+                id="functionDate"
+                name="functionDate"
+                className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+          </>
+        ) : hotelType == "restaurant" ? (
+          <>
+            <div className="flex">
+              <label className="block font-serif w-1/3 mb-3">
+                Check-In Time:
+              </label>
+              <input
+                type="time"
+                id="checkIn"
+                name="checkIn"
+                className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+            <div className="flex">
+              <label className="block font-serif w-1/3 mb-3">
+                Check-Out Time:
+              </label>
+              <input
+                type="time"
+                id="checkOut"
+                name="checkOut"
+                className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+          </>
+        ) : (
+          hotelType !== "restaurant" && (
+            <>
+              <div className="flex">
+                <label className="block font-serif w-1/3 mb-3">
+                  Check-In Date:
+                </label>
+                <input
+                  type="date"
+                  id="checkIn"
+                  name="checkIn"
+                  className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+                  required
+                />
+              </div>
+              <div className="flex">
+                <label className="block font-serif w-1/3 mb-3">
+                  Check-Out Date:
+                </label>
+                <input
+                  type="date"
+                  id="checkOut"
+                  name="checkOut"
+                  className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+                  required
+                />
+              </div>
+            </>
+          )
+        )}
 
         {/* Room Type (Hidden for Restaurants) */}
         {roomTypeVisible && (
-          <div className="mb-4">
-            <label htmlFor="roomType" className="block text-gray-700">
-              Room Type:
-            </label>
+          <div className="flex">
+            <label className="block font-serif w-1/3 mb-3">Room Type:</label>
             <select
               id="roomType"
               name="roomType"
-              className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+              className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
               required
             >
-              <option value="">--Select Room Type--</option>
+              <option value="">Select Room Type</option>
               <option value="standard">Standard</option>
               <option value="deluxe">Deluxe</option>
               <option value="suite">Suite</option>
@@ -126,84 +232,120 @@ const BookingForm = () => {
         )}
 
         {/* Number of Guests */}
-        <div className="mb-4">
-          <label htmlFor="guests" className="block text-gray-700">
+        <div className="flex">
+          <label className="block font-serif w-1/3 mb-3">
             Number of Guests:
           </label>
           <input
             type="number"
             id="guests"
             name="guests"
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+            className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+            min="1"
             required
           />
         </div>
 
         {/* Payment Details */}
-        <div className="mb-4">
-          <label htmlFor="payment" className="block text-gray-700">
-            Payment Method:
-          </label>
+        <div className="flex">
+          <label className="block font-serif w-1/3 mb-3">Payment Method:</label>
           <select
             id="payment"
             name="payment"
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
+            className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+            value={paymentMethod}
+            onChange={handlePaymentMethodChange}
             required
           >
-            <option value="">--Select Payment Method--</option>
+            <option value="">Select Payment Method</option>
             <option value="creditCard">Credit Card</option>
             <option value="paypal">PayPal</option>
-            <option value="bankTransfer">Bank Transfer</option>
+            <option value="bankTransfer">Online Transfer</option>
           </select>
         </div>
-        <div className="mb-4">
-          <label htmlFor="cardNumber" className="block text-gray-700">
-            Card Number:
-          </label>
-          <input
-            type="text"
-            id="cardNumber"
-            name="cardNumber"
-            className="w-full mt-1 p-2 border border-gray-300 rounded-md"
-            required
-          />
-        </div>
-        <div className="mb-4 flex gap-4">
-          <div>
-            <label htmlFor="expiry" className="block text-gray-700">
-              Expiry Date:
-            </label>
-            <input
-              type="text"
-              id="expiry"
-              name="expiry"
-              placeholder="MM/YY"
-              className="w-full mt-1 p-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="cvv" className="block text-gray-700">
-              CVV:
-            </label>
-            <input
-              type="text"
-              id="cvv"
-              name="cvv"
-              placeholder="3 digits"
-              className="w-full mt-1 p-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-        </div>
 
+        {/* Conditionally render fields based on the selected payment method */}
+        {paymentMethod === "creditCard" && (
+          <>
+            <div className="flex">
+              <label className="block font-serif w-1/3 mb-3">
+                Card Number:
+              </label>
+              <input
+                type="text"
+                id="cardNumber"
+                name="cardNumber"
+                className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+            <div className="flex gap-4">
+              <div className="w-1/2">
+                <label className="block font-serif mb-3 text-center">
+                  Expiry Date:{" "}
+                </label>
+                <input
+                  type="text"
+                  id="expiry"
+                  name="expiry"
+                  placeholder="MM/YY"
+                  className="w-full mt-1 mb-3 p-2 border border-gray-300 rounded-md text-center"
+                  required
+                />
+              </div>
+              <div className="w-1/2">
+                <label className="block font-serif mb-3 text-center">
+                  CVV:
+                </label>
+                <input
+                  type="text"
+                  id="cvv"
+                  name="cvv"
+                  placeholder="3 digits"
+                  className="w-full mt-1 mb-3 p-2 border border-gray-300 rounded-md text-center"
+                  required
+                />
+              </div>
+            </div>
+          </>
+        )}
+
+        {paymentMethod === "paypal" && (
+          <div className="flex">
+            <label className="block font-serif w-1/3 mb-3">PayPal Email:</label>
+            <input
+              type="email"
+              id="paypalEmail"
+              name="paypalEmail"
+              className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+        )}
+
+        {paymentMethod === "bankTransfer" && (
+          <div className="flex">
+            <label className="block font-serif w-1/3 mb-3">
+              Bank Account Number:
+            </label>
+            <input
+              type="text"
+              id="bankAccount"
+              name="bankAccount"
+              className="w-2/3 mt-1 mb-3 ml-1 p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+        )}
         {/* Submit Button */}
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
-        >
-          Confirm Booking
-        </button>
+        <div className="flex justify-center mt-5">
+          <button
+            type="submit"
+            className="w-1/3 bg-darkGold text-white p-2 rounded-md hover:opacity-90 justify-center"
+          >
+            Book Now
+          </button>
+        </div>
       </form>
     </div>
   );
